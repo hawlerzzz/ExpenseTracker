4570d989e717f8d01684c15e662050394b20b41d
Hawler A. Mohammed <hawleram@uia.no>
2025-09-18 09:38:02 +0200

    fixed import and routing issues

--- Frontend/expense-tracker/src/App.jsx
+++ Frontend/expense-tracker/src/App.jsx
@@ -7,8 +7,8 @@ import{
   Navigate,
 } from "react-router-dom";
 
-import Login from './pages/Auth/Auth/Login';
-import SignUp from './pages/Auth/Auth/SignUp';
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
 import Home from './pages/Dashboard/Home';
 import Income from './pages/Dashboard/Income';
 import Expenses from './pages/Dashboard/Expenses';
