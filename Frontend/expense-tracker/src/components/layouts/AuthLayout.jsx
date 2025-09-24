6710067a91700b681343d88dc3c1afe673c74713
Hawler A. Mohammed <hawleram@uia.no>
2025-09-24 13:35:11 +0200

    Update AuthLayout.jsx
    - Changed name

--- Frontend/expense-tracker/src/components/layouts/AuthLayout.jsx
+++ Frontend/expense-tracker/src/components/layouts/AuthLayout.jsx
@@ -6,7 +6,7 @@ import { LuTrendingUpDown } from 'react-icons/lu';
 const AuthLayout = ({ children }) => {
   return <div className='flex'>
     <div className='w-screen h-screen md:w-(60vw) px-12 pt-8 pb-12'>
-        <h2 className='text-lg font-medium text-black'>Expense Tracker</h2>
+        <h2 className='text-lg font-medium text-black'>Peak Finance</h2>
         {children}
     </div>
 
