57574aea3639ad4bd0fc918bff0ddb73de2f2f64
Hawler A. Mohammed <hawleram@uia.no>
2025-10-07 20:01:24 +0200

    update
    - authController.js : checks required fields, prevents duplicate emails, creates a user in MongoDB, and returns the user data with a generated token.
    
    -User.js : password hashing, method to compare password for login.

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,11 +1,44 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
+//Generate JWT token
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 };
 
-exports.registerUser = async (req, res) => {};
+// Register a new user
+exports.registerUser = async (req, res) => {
+    const { fullName, email, password } = req.body;
 
+    if (!fullName || !email || !password) {
+        return res.status(400).json({ message: 'Please provide all required fields' });
+    }
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Email is already in use, please use another one' });
+        }
+
+        const newUser = await User.create({
+            fullName,
+            email,
+            password,
+            profileImageUrl,
+        });
+
+        res.status(201).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id),
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Erorr registering user', error: error.message });
+    }
+
+};
+
+// Login user
 exports.loginUser= async (req, res) => {};
 
+// Get user info
 exports.getUserInfo = async (req, res) => {};
